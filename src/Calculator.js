64e5dbffc16ec41c05116a101e8946c51d9f1a7f
mikesmall <michael.l.a.small@gmail.com>
2017-12-12 18:05:55 -0500

    Re-adds nice default React header, plus some CSS

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class Calculator extends Component {
   render () {
     return (
       <div className="container">
-        <h1>Adding with React</h1>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Adding with React</h1>
+        </header>
+
+
         <div className="add">
-          <input type="text" />
+          <input type="text" /> <br />
           <span> + </span>
           <input type="text" />
           <span> = </span>
