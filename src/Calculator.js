b4dbbc290f9ef56609e9fd280b6bc4c75bf15284
mikesmall <michael.l.a.small@gmail.com>
2017-12-12 17:58:21 -0500

    Establishes Calculator.js

---
+++ src/Calculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Calculator extends Component {
+  render () {
+    return (
+      <div>
+        <p>Calculator component here.</p>
+      </div>
+    )
+  }
+
+}
+
+export default Calculator
